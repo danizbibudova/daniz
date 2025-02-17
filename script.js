09d10ea41930d18b872a668fbfaba4cac6f5bc87
danizbibudova <danizbeybudova@gmail.com>
2025-02-17 19:02:49 +0300

    tapsiriq bitti

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const massiv = ["squid game","america","write"]
+const massiv = ["12,9"]
 const cumle = massiv.join(", ");
 console.log(cumle);
+calculateAge(){
+    
+}
+    console.log.(cumle)
+
 
 
 
